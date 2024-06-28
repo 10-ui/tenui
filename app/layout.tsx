e60085b9b9fac2f8b7a190548d71007426bea6c6
10-ui <ui-10@outlook.jp>
2024-06-28 22:24:52 +0900

    レイアウトとコンポーネントの関数名の変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { fonts } from "@/utils/fonts";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/common/header";
+import { lc } from "@/utils/tw-lc";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='ja'>
+      <body className={lc(fonts.notoSansJP.className, "bg-gray-500")}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
