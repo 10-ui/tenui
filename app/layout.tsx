bdbf7d3fbdecfd44cd4872c0849971eca1dab1ba
10-ui <ui-10@outlook.jp>
2024-07-01 22:19:14 +0900

    favicon追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { fonts } from "@/utils/fonts";
 import Header from "@/components/common/header";
 import TopAnimation from "@/components/top/top-animation";
+import logo from "@/public/logo.png";
 import { lc } from "@/utils/tw-lc";
 import "./globals.css";
 
@@ -11,6 +12,13 @@ export const metadata: Metadata = {
     default: "Portfolio",
   },
   description: "Portfolio",
+  icons: [
+    {
+      rel: "icon",
+      url: logo.src,
+      sizes: "32x32",
+    },
+  ],
 };
 
 export default function RootLayout({
