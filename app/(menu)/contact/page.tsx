61e3df508738026c022086df3b8a047486ff33c2
10-ui <ui-10@outlook.jp>
2024-07-01 15:55:33 +0900

    フォーム変更

--- app/(menu)/contact/page.tsx
+++ app/(menu)/contact/page.tsx
@@ -1,3 +1,17 @@
+import ContactForm from "@/components/common/contact-form";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <main className='flex min-h-dvh flex-row'>
+      <div className='grid max-h-dvh w-full grid-cols-6'>
+        <div className='h-29 col-span-6 w-full rounded-3xl bg-main'></div>
+        <div className='col-span-2 flex h-[calc(100dvh-7.25rem)] basis-3/12 flex-col items-center justify-center rounded-3xl bg-main'>
+          <h2 className='text-center text-4xl font-bold'>Contact Form</h2>
+        </div>
+        <div className='col-span-4 flex h-[calc(100dvh-7.25rem)] basis-9/12 items-center justify-center rounded-3xl bg-main p-10'>
+          <ContactForm />
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ components/common/contact-form.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Textarea } from "@/components/ui/textarea";
+
+const contactSchema = z.object({
+  name: z.string().min(1, { message: "名前を入力してください" }),
+  email: z
+    .string()
+    .email({ message: "有効なメールアドレスを入力してください" }),
+  message: z.string().min(1, { message: "メッセージを入力してください" }),
+});
+
+const onSubmit = (data: z.infer<typeof contactSchema>) => {
+  console.log(data);
+};
+
+export default function ContactForm() {
+  const form = useForm<z.infer<typeof contactSchema>>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='mx-auto w-1/2 space-y-6'>
+        <h3 className='text-xl font-normal'>お問い合わせ</h3>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>名前</FormLabel>
+              <FormControl>
+                <Input
+                  type='text'
+                  placeholder='お名前を入力してください'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input
+                  type='email'
+                  placeholder='返信先のメールアドレスを入力してください'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='message'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>お問い合わせ内容</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder='お問い合わせ内容を入力してください'
+                  className='resize-none'
+                  rows={6}
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className='flex justify-center'>
+          <Separator className='my-4' />
+        </div>
+        <Button type='submit' className='w-full'>
+          送信
+        </Button>
+      </form>
+    </Form>
+  );
+}
