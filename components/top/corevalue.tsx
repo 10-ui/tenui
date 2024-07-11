d6fa6ac3377a3160a8df241386b77bce701fc5ab
10-ui <ui-10@outlook.jp>
2024-07-11 11:47:18 +0900

    corevalue変更

--- components/top/corevalue.tsx
+++ components/top/corevalue.tsx
@@ -8,40 +8,43 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function CoreValue() {
   useGSAP(() => {
-    gsap.from("#core-value", {
-      scale: 0.8,
-      x: -250,
+    gsap.set("#core-value", {
+      scale: 0.5,
+      x: 980,
+      y: -460,
+    });
+    gsap.set(".core-value-text", {
       opacity: 0,
+      y: -50,
+    });
+    gsap.set("#core-value-title", {
+      opacity: 0,
+    });
+    gsap.to("#core-value", {
+      x: 0,
+      y: 0,
+      scale: 1,
+      scrollTrigger: {
+        start: "20% 60%",
+        end: "40% center",
+        scrub: true,
+      },
+    });
+    gsap.to("#core-value-title", {
+      opacity: 1,
       scrollTrigger: {
         start: "35% 60%",
+        end: "40% center",
+        scrub: true,
+      },
+    });
+    gsap.to(".core-value-text", {
+      y: 0,
+      opacity: 1,
+      stagger: 0.6,
+      scrollTrigger: {
+        start: "40% 60%",
         end: "45% center",
-        scrub: true,
-      },
-    });
-    gsap.from("#core-value-title", {
-      y: 150,
-      opacity: 0,
-      scrollTrigger: {
-        start: "35% 60%",
-        end: "50% center",
-        scrub: true,
-      },
-    });
-    gsap.from("#core-value-text1", {
-      y: 150,
-      opacity: 0,
-      scrollTrigger: {
-        start: "45% 60%",
-        end: "50% center",
-        scrub: true,
-      },
-    });
-    gsap.from("#core-value-text2", {
-      y: 150,
-      opacity: 0,
-      scrollTrigger: {
-        start: "45% 60%",
-        end: "50% center",
         scrub: true,
       },
     });
@@ -49,7 +52,7 @@ export default function CoreValue() {
   return (
     <div className='flex min-h-screen flex-row items-center justify-center rounded-4xl bg-main p-28'>
       <div className='flex w-full items-start justify-between'>
-        <h2 className='mb-16 text-3xl font-bold' id='core-value'>
+        <h2 className='mb-16 overflow-clip text-3xl font-bold' id='core-value'>
           作品制作に掛ける思い
         </h2>
         <div className='flex flex-col'>
@@ -57,10 +60,10 @@ export default function CoreValue() {
             「四方良し」のWeb制作
           </p>
           <br />
-          <p className='text-xl' id='core-value-text1'>
+          <p className='core-value-text text-xl'>
             みんなが気持ちよく、心を一つに問題解決に向かっていけるよう
           </p>
-          <p className='text-xl' id='core-value-text2'>
+          <p className='core-value-text text-xl'>
             一緒に考えることができるエンジニアになりたいと考えています。
           </p>
         </div>
